46733abdb376b86422b01a01cc85ef50608c1aee
Melvin Sommer <sommer.melvin@gmail.com>
2026-07-09 18:39:50 +0200

    Study 21: Noise Floor Ground Dissolution

---
+++ 21_noise_ground.rb
@@ -0,0 +1,93 @@
+use_bpm 72
+use_external_synths true
+use_synth :kestrel_wraith
+play 52, attack: 0.4, release: 1.8, amp: 0.35, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.22, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.12, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.35, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.28, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.35, pan: 0
+sleep 3
+play 62, attack: 0.6, release: 2.0, amp: 0.55, cutoff: 78, detune: 5, noise_mix: 0.02, res: 0.30, pan: 0
+sleep 4
+play 62, attack: 0.4, release: 1.8, amp: 0.50, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.30, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.26, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.14, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.20, cutoff: 80, detune: 5, noise_mix: 0.02, res: 0.35, pan: 0
+sleep 3
+play 52, attack: 0.6, release: 2.2, amp: 0.42, cutoff: 78, detune: 5, noise_mix: 0.02, res: 0.30, pan: 0
+sleep 5
+use_synth :kestrel_wraith
+play 52, attack: 0.4, release: 1.8, amp: 0.35, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.22, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.12, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.35, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.28, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.35, pan: 0
+sleep 3
+play 62, attack: 0.6, release: 2.0, amp: 0.55, cutoff: 78, detune: 5, noise_mix: 0.15, res: 0.30, pan: 0
+sleep 4
+play 62, attack: 0.4, release: 1.8, amp: 0.50, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.30, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.26, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.14, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.20, cutoff: 80, detune: 5, noise_mix: 0.15, res: 0.35, pan: 0
+sleep 3
+play 52, attack: 0.6, release: 2.2, amp: 0.42, cutoff: 78, detune: 5, noise_mix: 0.15, res: 0.30, pan: 0
+sleep 5
+use_synth :kestrel_wraith
+play 52, attack: 0.4, release: 1.8, amp: 0.35, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.22, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.12, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.35, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.28, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.35, pan: 0
+sleep 3
+play 62, attack: 0.6, release: 2.0, amp: 0.55, cutoff: 78, detune: 5, noise_mix: 0.45, res: 0.30, pan: 0
+sleep 4
+play 62, attack: 0.4, release: 1.8, amp: 0.50, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.30, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.26, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.14, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.20, cutoff: 80, detune: 5, noise_mix: 0.45, res: 0.35, pan: 0
+sleep 3
+play 52, attack: 0.6, release: 2.2, amp: 0.42, cutoff: 78, detune: 5, noise_mix: 0.45, res: 0.30, pan: 0
+sleep 5
+use_synth :kestrel_wraith
+play 52, attack: 0.4, release: 1.8, amp: 0.35, cutoff: 80, detune: 5, noise_mix: 0.85, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.22, cutoff: 80, detune: 5, noise_mix: 0.85, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.12, cutoff: 80, detune: 5, noise_mix: 0.85, res: 0.35, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.28, cutoff: 80, detune: 5, noise_mix: 0.85, res: 0.35, pan: 0
+sleep 3
+play 62, attack: 0.6, release: 2.0, amp: 0.55, cutoff: 78, detune: 5, noise_mix: 0.85, res: 0.30, pan: 0
+sleep 4
+use_synth :kestrel_wraith
+play 62, attack: 0.4, release: 1.8, amp: 0.22, cutoff: 80, detune: 5, noise_mix: 1.0, res: 0.30, pan: 0
+sleep 3
+play 59, attack: 0.4, release: 1.8, amp: 0.12, cutoff: 80, detune: 5, noise_mix: 1.0, res: 0.35, pan: 0
+sleep 3
+play 57, attack: 0.4, release: 1.8, amp: 0.06, cutoff: 80, detune: 5, noise_mix: 1.0, res: 0.35, pan: 0
+sleep 3
+play 55, attack: 0.4, release: 1.8, amp: 0.03, cutoff: 80, detune: 5, noise_mix: 1.0, res: 0.35, pan: 0
+sleep 3
+play 52, attack: 0.6, release: 2.2, amp: 0.00, cutoff: 78, detune: 5, noise_mix: 1.0, res: 0.30, pan: 0
+sleep 5
+use_synth :kestrel_dust
+play 52, attack: 1.5, release: 5.0, amp: 0.08, cutoff: 48, res: 0.20, pan: 0, noise_mix: 1.0
+sleep 6
+use_synth :kestrel_dust
+play 52, attack: 2.0, release: 7.0, amp: 0.05, cutoff: 45, res: 0.15, pan: 0, noise_mix: 1.0
+sleep 8
